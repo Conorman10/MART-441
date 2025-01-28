863674854818f862d5674bdea4d06c5e55f0a3f0
Conorman10 <cm161761@umconnect.umt.edu>
2025-01-27 19:57:10 -0700

    asdf

--- Homework 2/script.js
+++ Homework 2/script.js
@@ -1,2 +1,10 @@
 console.log("My 3 Favorite Websites:");
 console.log("https://doodles.google/ ; https://ncase.me/trust/ ; https://www.imdb.com/")
+console.log("")
+console.log("My 3 Favorite Games:")
+console.log("Cyberpunk 2077 ; Raft Wars ; Poker")
+console.log("")
+console.log("My 3 Favorite Artists:")
+console.log("1. Jimi Hendrix: Best guitar player this world has ever seen, and he went to my high school")
+console.log("2. Denis Villenueve: My favorite movie director, director of the Dune movies and Arrival, personal hero")
+console.log("3. David Bowie: One of my mother's favorite movie artists, a big reason why she is sucha beautiful, happy, and lovely person")
