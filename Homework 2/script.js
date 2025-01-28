771a6dce4cdef63fcfd176f9a6cf4f04b3314854
Conorman10 <cm161761@umconnect.umt.edu>
2025-01-27 20:03:32 -0700

    asdf

--- Homework 2/script.js
+++ Homework 2/script.js
@@ -8,3 +8,6 @@ console.log("My 3 Favorite Artists:")
 console.log("1. Jimi Hendrix: Best guitar player this world has ever seen, and he went to my high school")
 console.log("2. Denis Villenueve: My favorite movie director, director of the Dune movies and Arrival, personal hero")
 console.log("3. David Bowie: One of my mother's favorite movie artists, a big reason why she is sucha beautiful, happy, and lovely person")
+
+var name = prompt('what is your name?;')                  
+console.log('hello,' + Conor)
