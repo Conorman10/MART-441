651094596645cacaadd10fceb0ed304177eda22e
Conorman10 <cm161761@umconnect.umt.edu>
2025-02-03 20:29:20 -0700

    adf

---
+++ Homework 3/main.js
@@ -0,0 +1,7 @@
+function storyFunction(choice) {
+    var answer1 = document.getElementById("choice1").innerHTML;
+    var answer2 = document.getElementById("choice2").innerHTML;
+    if (choice == 1 && answer1 == "Yes") {
+        
+    }
+}
