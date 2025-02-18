f1865876ab353cdf65ad9a5e4ef40bea2d8576ee
Conorman10 <cm161761@umconnect.umt.edu>
2025-02-18 14:41:18 -0700

    jkhb

--- Homework 5/script.js
+++ Homework 5/script.js
@@ -1,10 +1,36 @@
+var imageTags = ["image1", "image2", "image3", "image4", "image5", "image6"];
+var blankImagePath = "images/blank.jpg";
+var actualImages = [];
+
 function printBlankArray()
 {
     createRandomImageArray();
+    var gameBoard = document.getElementById("gameBoard");
+    gameBoard.innerHTML = "";
     for(var i = 0; i < imageTags.length; i++)
     {
-        document.getElementById(imageTags[i]).src= blankImagePath;
-    }    
+        var img = document.createElement("img");
+        //This is where I left OFF!!!!!!
+    }
 }
 
 function createRandomImageArray()
+{ 
+    var actualImagePath = ["images/rock.jpg", "images/paper.png", "images/scissors.jpg"];
+    var count = [0,0];
+    while(actualImages.length < 12)
+    {
+        var randomNumber = Math.floor(Math.random() * actualImagePath.length)
+        if(count[randomNumber] < 3)
+            actualImages.push(actualImagePath[randomNumber]);
+            count[randomNumber] = count[randomNumber] +1
+    }
+}
+
+function flipImage(number)
+{
+    document.getElementById(imageTags[number]).src= actualImages[number];
+}
+
+
+
