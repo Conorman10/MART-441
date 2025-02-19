a4baac031d2f80be46a8c07295676ed16dbe5ff4
Conorman10 <cm161761@umconnect.umt.edu>
2025-02-18 17:51:13 -0700

    adf

--- Homework 5/script.js
+++ Homework 5/script.js
@@ -1,4 +1,4 @@
-var imageTags = ["image1", "image2", "image3", "image4", "image5", "image6"];
+var imageTags = ["image1", "image2", "image3", "image4", "image5", "image6", "image7", "image8", "image9", "image10", "image11", "image12"];
 var blankImagePath = "images/blank.jpg";
 var actualImages = [];
 
@@ -10,26 +10,40 @@ function printBlankArray()
     for(var i = 0; i < imageTags.length; i++)
     {
         var img = document.createElement("img");
-        //This is where I left OFF!!!!!!
+        img.src = blankImagePath;
+        img.id = imageTags[i];
+        img.dataset.index = i;
+        img.onclick = function ()
+        {
+            flipImage(this.id);
+        };
+        gameBoard.appendChild(img);
     }
 }
 
 function createRandomImageArray()
 { 
     var actualImagePath = ["images/rock.jpg", "images/paper.png", "images/scissors.jpg"];
-    var count = [0,0];
+    var count = [0, 0, 0];
     while(actualImages.length < 12)
     {
-        var randomNumber = Math.floor(Math.random() * actualImagePath.length)
-        if(count[randomNumber] < 3)
+        var randomNumber = Math.floor(Math.random() * actualImagePath.length);
+        if(count[randomNumber] < 4) 
+        {
             actualImages.push(actualImagePath[randomNumber]);
-            count[randomNumber] = count[randomNumber] +1
+            count[randomNumber]++;
+        }
     }
 }
 
-function flipImage(number)
+function flipImage(id)
 {
-    document.getElementById(imageTags[number]).src= actualImages[number];
+    var index = imageTags.indexOf(id);
+    if (index !== -1)
+    {
+        document.getElementById(id).src = actualImages[index];
+
+    }
 }
 
 
