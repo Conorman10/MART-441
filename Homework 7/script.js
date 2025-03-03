fe5bd7fe0af4a957d83db0ea469a09502618d417
Conorman10 <cm161761@umconnect.umt.edu>
2025-03-03 08:27:58 -0700

    jkhf

---
+++ Homework 7/script.js
@@ -0,0 +1,13 @@
+class viewFinder {
+    constructor(title) {
+        this.title = title;
+    }    
+
+    get theTitle () {
+        return this.title;
+    }
+    toString() {
+        return this.title;
+    }    
+
+}
