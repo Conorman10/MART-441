7201271a89de2804fdb732ebe7cc4a10ec6cb51c
Conorman10 <cm161761@umconnect.umt.edu>
2025-03-03 23:07:54 -0700

    giug

--- Homework 7/main.js
+++ Homework 7/main.js
@@ -1,7 +1,5 @@
-
-var myViewArray = newArray();
-
-class viewFinder {
+var myArtworkArray = newArray();
+class Artwork {
     constructor(title, image, description, author, year) {
         this.title = title;
         this.image = image;
@@ -10,15 +8,66 @@ class viewFinder {
         this.year = year;
     }    
 
-    get theTitle () {
+    toString()
+    { 
+        return 'This painting is called The ${this.title}. <br /> <br /> ${this.image}. <br /> This painting is ${this.description}. It was created by ${this.author} in ${this.year}'
+    }
+
+    get theTitle()
+    { 
         return this.title;
     }
-    toString() {
-        return this.title;
-    }    
 
+    get theImage()
+    {
+        return this.image;
+    }
 }
 
+
+
 function initializeArray() {
-    var myViewFinder = new viewFinder("A really funny Pug");
+
+    var myArtwork = 
+        new Artwork(
+            "Mona Lisa",
+            "images/monaLisa.webp",
+            "A portrait of a woman with a mysterious style in front of a valley.",
+            "Leonardo da Vinci",
+            1503
+        ),
+        new Artwork(
+            "Starry Night",
+            "images/starryNight.webp",
+            "A painting of the stars swirling in the night sky.",
+            "Vincent van Gogh",
+            1889
+        ),
+        new Artwork (
+            "Last Supper",
+            "images/theLastSupper.webp",
+            "An artistic rendition of the last time Jesus ate before his death.",
+            "Leonardo da Vinci",
+            1495
+        ),
+        new Artwork (
+            "Scream",
+            "images/scream.webp",
+            "A picture of a strange being standing on a bridge holding his ears and screaming.",
+            "Edvard Munch",
+            1893
+        )
+        new Artwork (
+            "Girl with a Peal Earring",
+            "images/pearlEarring.webp",
+            "A portrait of a young girl wearing a Pearl earring.",
+            "johannes Vermeer",
+            1665
+        )
+            
+}
+
+function accessInformation() {
+    var randomNumber = Math.floor(Math.random() * myViewFinderArray.length);
+    document.getElementById("title").innerHTML = myViewFinderArray[randomNumber].toString();
 }
